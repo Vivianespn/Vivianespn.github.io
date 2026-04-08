e3168b6aad42e66a0fb971221c8f811e9e051599
Viviane Novo <viviane.novo.702@ufrn.edu.br>
2026-04-08 02:38:38 -0300

    fix: Corrige maiúsculas dos componentes e configura permissões

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import Home from '../views/home.vue';
-import About from '../views/about.vue';
-import Projects from '../views/projects.vue';
-import Contact from '../views/contact.vue';
+import Home from '../views/Home.vue';
+import About from '../views/About.vue';
+import Projects from '../views/Projects.vue';
+import Contact from '../views/Contact.vue';
 
 const routes = [
   { path: '/', name: 'Home', component: Home },
